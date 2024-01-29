9ceeee1f26b268e0a0e02fa4e345c28d3e66c37d
unknown <CIAC@CIAC2-008.academic.mixcoac.upmx.mx>
2024-01-29 09:53:25 -0600

    Practica1 29-01-2024

---
+++ Practica1/Practica1/kernel.cu
@@ -0,0 +1,19 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+__global__ void print_hello_cuda()
+{
+    int i = threadIdx.x;
+    printf("[DEVICE] ThreadIdx.x: %d \n", i);
+}
+
+int main()
+{
+    print_hello_cuda << <2, 8 >> > ();
+    return 0;
+}
